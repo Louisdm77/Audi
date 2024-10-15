afea9f0f99404b758ec00409594d0b37a9326ffa
Akwaji Dan Uke <danakwaji3@gmail.com>
2024-10-15 14:25:34 +0100

    testing

--- src/assets/models.jsx
+++ src/assets/models.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import "../assets/model.css";
-const models = () => {
+
+const Models = () => {
   return (
     <div>
-      <h2 style={{ fontWeight: "bold", fontSize: "40px" }}>
-        Discover our models
-      </h2>
-      <div className="row imgholder md:w-full" style={{ textAlign: "start" }}>
-        <div className="img w-full p-6 border-box">
+      <h2 className="title">Discover our models</h2>
+      <div className="imgholder">
+        <div className="img-container">
           <img
-            style={{ width: "100%", height: "auto" }}
-            src="https://img.freepik.com/free-photo/black-jeep-is-parked-desert-with-other-cars_1340-35824.jpg?t=st=1728725022~exp=1728728622~hmac=4afa6a30edbed95eea6c3a26c5fb43bf258f781072f2ffad45691bc2fe982f6b&w=740"
+            src="https://img.freepik.com/free-photo/silver-sedan-with-red-lights-parked-port_114579-4385.jpg?t=st=1728998070~exp=1729001670~hmac=34f1e1a6130cb10ffacc0cae8dc5e5cd6215e058d0cb8094edeb87c584693c2a&w=740"
             alt=""
+            className="img"
           />
         </div>
-        <div className="w-54 p-14 border-box">
-          <h3 className=" font-bold text-xl mb-4">
+        <div className="content">
+          <h3 className="font-bold text-xl mb-4">
             "Discover your perfect match at Audi, where every need is met."
           </h3>
           <p>
@@ -23,12 +22,11 @@ const models = () => {
             vehicle for every requirement. Discover and configure our series
             models now.
           </p>
-          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-4 mt-6 px-6 border border-gray-400 rounded shadow">
-            Checkout our models
-          </button>
+          <button className="btn mt-5">Checkout our models</button>
         </div>
       </div>
     </div>
   );
 };
-export default models;
+
+export default Models;
